87f1bf0f4383da91d0611c424dea384264460b35
ualehosaini2019 <ualehosaini2019@hotmail.com>
2019-12-23 21:17:32 +0000

    IsValidDate and IsValidDate_Valid methods has been added

--- src/Aqua.DateTimeHelpers/DateTimeHelpers.cs
+++ src/Aqua.DateTimeHelpers/DateTimeHelpers.cs
@@ -1,6 +1,25 @@
-﻿namespace Aqua.DateTimeHelpers
+﻿using System;
+
+namespace Aqua.DateTimeHelpers
 {
     public class DateTimeHelpers
     {
+        /// <summary>
+        /// Check the combination of Year, Month, and Day is a Valid Date
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public static bool IsValidDate(int year, int month, int day)
+        {
+            return
+                year >= DateTime.MinValue.Year &&
+                year <= DateTime.MaxValue.Year &&
+                month >= 1 &&
+                month <= 12 &&
+                day >= 1 &&
+                DateTime.DaysInMonth(year, month) >= day;
+        }
     }
 }
--- tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
+++ tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
@@ -1,7 +1,16 @@
+using Xunit;
+
 namespace Aqua.DateTimeHelpers.Tests
 {
     public class DateTimeHelpersTests
     {
-
+        [Theory]
+        [InlineData(true, 2017, 1, 1)]
+        [InlineData(false, 2017, 10, 35)]
+        [InlineData(false, 2017, 13, 1)]
+        public void IsValidDate_Valid(bool expected, int year, int month, int day)
+        {
+            Assert.Equal(expected, DateTimeHelpers.IsValidDate(year, month, day));
+        }
     }
 }
