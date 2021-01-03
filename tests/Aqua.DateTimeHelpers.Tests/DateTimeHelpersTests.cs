5017ce2a704b66df434dee2155a43fa6d4b4f5ac
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-03 21:32:11 +0000

    Modernized a test method

--- tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
+++ tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
@@ -122,10 +122,7 @@ namespace Aqua.DateTimeHelpers.Tests
         [InlineData(false, 2019, 2, 10)]
         [InlineData(false, 2017, 10, 15)]
         [InlineData(true, 2019, 8, 14)]
-        public void IsWednesday_Valid(bool expected, int year, int month, int day)
-        {
-            Assert.Equal(expected, DateTimeHelpers.IsWednesday(new DateTime(year, month, day)));
-        }
+        public void IsWednesday_Valid(bool expected, int year, int month, int day) => Assert.Equal(expected, DateTimeHelpers.IsWednesday(new DateTime(year, month, day)));
 
         [Theory]
         [InlineData(false, 2019, 2, 10)]
