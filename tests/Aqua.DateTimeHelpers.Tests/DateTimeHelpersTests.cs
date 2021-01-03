41145b05f7005f42caa02212750635bc3b7453f0
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-03 21:33:43 +0000

    Modernized a test method

--- tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
+++ tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
@@ -225,10 +225,7 @@ namespace Aqua.DateTimeHelpers.Tests
 
         [Theory(Skip = "culture difference")]
         [MemberData(nameof(GetFirstDayOfNextWeekData))]
-        public void GetFirstDayOfNextWeek_Valid(DateTime expected, DateTime dateTime)
-        {
-            Assert.Equal(expected, DateTimeHelpers.GetFirstDayOfNextWeek(dateTime));
-        }
+        public void GetFirstDayOfNextWeek_Valid(DateTime expected, DateTime dateTime) => Assert.Equal(expected, DateTimeHelpers.GetFirstDayOfNextWeek(dateTime));
 
         public static IEnumerable<object[]> GetFirstDayOfNextWeekData =>
             new List<object[]>
