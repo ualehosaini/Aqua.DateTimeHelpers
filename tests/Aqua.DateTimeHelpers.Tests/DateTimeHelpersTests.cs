403a112c970575b5ff99c1aadd457d1569f2c315
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-08 22:18:19 +0000

    Corrected naming

--- tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
+++ tests/Aqua.DateTimeHelpers.Tests/DateTimeHelpersTests.cs
@@ -210,12 +210,12 @@ namespace Aqua.DateTimeHelpers.Tests
             };
 
         [Theory]
-        [MemberData(nameof(GetGenerateDateListData()))]
+        [MemberData(nameof(GenerateDateListData))]
         public void GenerateDateList_Valid(IEnumerable<DateTime> expected,
             DateTime fisrtDate,
             DateTime lastDate) => Assert.Equal(expected, DateTimeHelpers.GenerateDateList(fisrtDate, lastDate));
 
-        public static IEnumerable<object[]> GetGenerateDateListData()
+        public static IEnumerable<object[]> GenerateDateListData()
         {
             return new List<object[]>
                 {
